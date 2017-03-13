e7f9e69e663a5759a20128b772f0004a696f7361
Darkoe <k.x.cai@163.com>
2017-03-13 21:45:38 +0800

    finish a basic use

--- taskAtHand.js
+++ taskAtHand.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function MyApp()
+function TaskAtHandApp()
 {
     var version = "v1.0";
 
@@ -9,9 +9,35 @@ function MyApp()
     }
 
     this.start = function(){
+        $("#new-task-name").keypress(function(e) {
+            if (e.which === 13) {
+                addTask();
+                return false;
+            }
+        })
+        .focus();
+    function addTask(){
+        var taskName = $("#new-task-name").val();
+        if(taskName){
+            addTaskElement(taskName);
+            $("#new-task-name").val("").focus;
+        }
+
+    }
+    function addTaskElement(taskName){
+        var $task = $("<li></li>");
+        $task.text(taskName);
+        $("#task-list").append($task);
+    }
         $("#app>header").append(version);
         setStatus("ready");
     };
 }
 
+
+
+$(function(){
+    window.app = new TaskAtHandApp();
+    window.app.start();
+})
 
